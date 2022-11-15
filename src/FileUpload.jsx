55c786060be4640d6d5eaa1f8d3fe3860f0a3f5e
Adam Zakaria <azakaria@Adams-MacBook-Air.local>
2022-11-15 15:03:33 -0500

    File upload has the three columns and the uploaded file names are displayed in the correct columns. Lets refactor it for later

--- src/FileUpload.jsx
+++ src/FileUpload.jsx
@@ -2,54 +2,80 @@ import React, { useState, useEffect } from 'react';
 import './FileUpload.scss'
 
 function FileUpload() {
-    let deleteFile = (e) => {
-        //setFileData()
-        setFileData({})
-        fileData = ""
-        console.log('delete')
-        //e.target.file
-        //maybe this needs a ref to the corresponding input
+
+    //TODO Implement Remove, by setting file to empty string
+    const [products, setProducts] = useState({});
+    const [approvals, setApprovals] = useState({});
+    const [others, setOthers] = useState({});
+    let handleProducts = (e) => {
+        setProducts(e.target.files[0])
     }
-    const [fileData, setFileData] = useState({});
-    let handleFileInput = (e) => {
-        setFileData(e.target.files[0])
-//console.log('onChange');
-//console.log(`e.target.files[0]: ${e.target.files[0]}`);
-//console.log(`e.target.files[0]: ${JSON.stringify(e.target.files[0])}`);
-console.log(`e.target.files[0]: ${e.target.files[0].name}`);
+    let handleApprovals = (e) => {
+        setApprovals(e.target.files[0])
+    }
+    let handleOthers = (e) => {
+        setOthers(e.target.files[0])
     }
 
     return (
-<div>
-    <div className='uploaded'>
-        { fileData ? 
-            <>
-            <span>{fileData.name}</span>
-            <span><a className='delete' href='/delete'>Remove</a></span>
-            </>
-        : null
-        }
-    </div>
+        <>
+        <div>Current Attachments:</div>
+        <div className='currentAttachments'>
+            <div className='flex-item'>
+                Products
+                <div className='filesUploaded'>
+                    {products ?
+                        <>
+                            <span>{products.name}</span>
+                            <span><a className='delete' href='/delete'>Remove</a></span>
+                        </>
+                        : null
+                    }
+                </div>
 
-<input onClick={()=> console.log('Click!')} onChange={handleFileInput} type="file" name="file" id="file" className="inputfile" />
+                <input onClick={() => console.log('Click!')} onChange={handleProducts} type="file" name="file0" id="file0" className="inputfile" />
 
-<label className='uploadFileBtn' htmlFor="file">Upload a file</label>
+                <label className='uploadFileBtn' htmlFor="file0">Upload a file</label>
 
-</div>
+            </div>
+            <div className='flex-item'>
+               Approvals 
+                <div className='filesUploaded'>
+                    {approvals ?
+                        <>
+                            <span>{approvals.name}</span>
+                            <span><a className='delete' href='/delete'>Remove</a></span>
+                        </>
+                        : null
+                    }
+                </div>
+
+                <input onChange={handleApprovals} type="file" name="file1" id="file1" className="inputfile" />
+
+                <label className='uploadFileBtn' htmlFor="file1">Upload a file</label>
+
+            </div>
+
+            <div className='flex-item'>
+                Others
+                <div className='filesUploaded'>
+                    {others ?
+                        <>
+                            <span>{others.name}</span>
+                            <span><a className='delete' href='/delete'>Remove</a></span>
+                        </>
+                        : null
+                    }
+                </div>
+
+                <input onClick={() => console.log('Click!')} onChange={handleOthers} type="file" name="file2" id="file2" className="inputfile" />
+
+                <label className='uploadFileBtn' htmlFor="file2">Upload a file</label>
+
+            </div>
+        </div>
+        </>
     );
 }
 
 export default FileUpload;
-
-{/*
-<div>
-    <form action="">
-    <input onChange={handleFileInput} type="file" id="myFile" name="filename" />
-    <input type="submit" />
-    </form>
-    <div>
-    <button className='delete' onClick={() => deleteFile(fileData)}></button>
-</div>
-</div>
-
-*/}
